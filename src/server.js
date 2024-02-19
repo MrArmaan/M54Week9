b27237097f60cc3dc8a1c8b317c4a7bc18def349
Mohammed Armaan <mr_mohammed_armaan@icloud.com>
2024-02-19 11:27:09 +0000

    Enhancement: Called user function

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 
+const User = require("./users/model");
+
 const port = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
 
+const synceTables = () => {
+  User.sync();
+};
+
 app.listen(port, () => {
+  synceTables();
   console.log(`Server is listening on port ${port}`);
 });
